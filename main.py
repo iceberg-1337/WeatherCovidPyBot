4e4bef1edf9b3280d173518b3f7967b88b536401
Iceberg-1337 <egor.loktyushin@gmail.com>
2021-04-12 11:05:49 +0300

    bot polling

--- main.py
+++ main.py
@@ -52,3 +52,15 @@ def action(message):
             bot.register_next_step_handler(message, get_covid)
         else:
             bot.send_message(message.chat.id, 'Я не знаю что ответить 😢')
+
+# Enable saving next step handlers to file "./.handlers-saves/step.save".
+# Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
+# saving will hapen after delay 2 seconds.
+bot.enable_save_next_step_handlers(delay=2)
+
+# Load next_step_handlers from save file (default "./.handlers-saves/step.save")
+# WARNING It will work only if enable_save_next_step_handlers was called!
+bot.load_next_step_handlers()
+
+# RUN
+bot.polling(none_stop=True)
