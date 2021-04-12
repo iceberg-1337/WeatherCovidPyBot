b40edcc929b9f23da1a461c60082ecc941db7f44
Iceberg-1337 <egor.loktyushin@gmail.com>
2021-04-12 11:02:20 +0300

    welcome message

--- main.py
+++ main.py
@@ -15,3 +15,19 @@ config_dict = get_default_config()
 config_dict['language'] = 'ru'
 owm = pyowm.OWM('4d21785a4841673ea7889b3d412f2681', config_dict)
 mgr = owm.weather_manager()
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)
+    item1 = types.KeyboardButton('☁ Узнать погоду ☀')
+    item2 = types.KeyboardButton('🦠 Узнать про Covid-19 🦠')
+
+    markup.add(item1, item2)
+
+
+
+    bot.send_message(message.chat.id,
+    "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы быть подопытным кроликом. \n что желаете сделать".format(
+    message.from_user, bot.get_me()),
+    parse_mode='html', reply_markup=markup)
+    bot.register_next_step_handler(message, action)
