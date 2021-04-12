bf0b80ebfece63ecbc8ea1c00258184d3541bfd6
halmg <iakovlev.vadim@list.ru>
2021-04-12 13:18:40 +0300

    add code in covid file

--- covid.py
+++ covid.py
@@ -5,3 +5,37 @@ covid19 = COVID19Py.COVID19()
 
 
 bot = telebot.TeleBot('1655397111:AAGOqLwVAicRXDlvic8nYvJ9gSPhBgDV1xc')
+
+
+def get_covid(message):
+    final_message = ""
+    get_message_bot = message.text.strip().lower()
+    if get_message_bot == "сша":
+        location = covid19.getLocationByCountryCode("US")
+    elif get_message_bot == "украина":
+        location = covid19.getLocationByCountryCode("UA")
+    elif get_message_bot == "россия":
+        location = covid19.getLocationByCountryCode("RU")
+    elif get_message_bot == "беларусь":
+        location = covid19.getLocationByCountryCode("BY")
+    elif get_message_bot == "казакхстан":
+        location = covid19.getLocationByCountryCode("KZ")
+    elif get_message_bot == "италия":
+        location = covid19.getLocationByCountryCode("IT")
+    elif get_message_bot == "франция":
+        location = covid19.getLocationByCountryCode("FR")
+    elif get_message_bot == "германия":
+        location = covid19.getLocationByCountryCode("DE")
+    elif get_message_bot == "япония":
+        location = covid19.getLocationByCountryCode("JP")
+
+    if final_message == "":
+        date = location[0]['last_updated'].split("T")
+        time = date[1].split(".")
+        final_message = f"<u>Данные по стране:</u>\nНаселение: {location[0]['country_population']:,}\n" \
+                        f"Последнее обновление: {date[0]} {time[0]}\nПоследние данные:\n<b>" \
+                        f"Заболевших: </b>{location[0]['latest']['confirmed']:,}\n<b>Сметрей: </b>" \
+                        f"{location[0]['latest']['deaths']:,}"
+
+
+    bot.send_message(message.chat.id, final_message, parse_mode='html')
