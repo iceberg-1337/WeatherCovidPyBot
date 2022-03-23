f2f3157c66be7c99e6ac3194a709faaa1cd8be05
iceberg-1337 <70839369+iceberg-1337@users.noreply.github.com>
2022-03-23 12:08:40 +0300

    Update wearher.py

--- wearher.py
+++ wearher.py
@@ -3,7 +3,7 @@ from pyowm.utils.config import get_default_config
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('1655397111:AAGOqLwVAicRXDlvic8nYvJ9gSPhBgDV1xc')
+bot = telebot.TeleBot('Token')
 
 
 def get_weather(message):
